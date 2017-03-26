0326f10a6c567e450559be51911b10e465083f5d
Nathan Douds <nate.douds@gmail.com>
2017-03-26 08:07:05 -0500

    adding styling

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -7,7 +7,11 @@ import {Component, Input} from '@angular/core'
             <!--{{event.name}} is interpolation -> 1-way binding, looking for the events-list components class, and then looks for event on that object-->
             <h2>{{event?.name}}</h2>
             <div>Date: {{event?.date}}</div>
-            <div>Time: {{event?.time}}</div>
+            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
+            </div>
             <div>Price: \${{event?.price}}</div>
             <div *ngIf="event?.location">
                 <span>Location: {{event?.location?.address}}</span>
@@ -22,8 +26,15 @@ import {Component, Input} from '@angular/core'
     .thumbnail {min-height: 210px;}
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb}
+    .green {color: #003300 !important;}
+        .bold {font-weight: bold}
     `]
 })
 export class EventThumbnailComponent {
     @Input() event:any
+
+    getStartTimeClass() {
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+        return {green: isEarlyStart, bold: isEarlyStart}
+    }
 }
