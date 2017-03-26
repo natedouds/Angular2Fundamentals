e08123ecbff736adf0aac02dfcc998addc0d81af
Nathan Douds <nate.douds@gmail.com>
2017-03-26 08:47:24 -0500

    adding custom and third party services

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,12 +6,15 @@ import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
+import { EventService } from './shared/event.service'
+import { ToastrService } from "./common/toastr.service";
 
 @NgModule({
     imports: [BrowserModule],
     //components are declared within a module
     declarations: [EventsAppComponent, EventsListComponent, EventThumbnailComponent, NavBarComponent], //if you forget this, you'll see an error that says this is not a known element
     //this is the component that bootstraps our module
-    bootstrap: [EventsAppComponent]
+    bootstrap: [EventsAppComponent],
+    providers: [EventService, ToastrService]
 })
 export class AppModule {}
