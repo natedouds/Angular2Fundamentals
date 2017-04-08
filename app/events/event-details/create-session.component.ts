6f036ad842e7708610c295aa14bd5f58ad6eede6
Nathan Douds <nate.douds@gmail.com>
2017-04-08 07:33:20 -0500

    custom validator

--- app/events/event-details/create-session.component.ts
+++ app/events/event-details/create-session.component.ts
@@ -30,7 +30,8 @@ export class CreateSessionComponent implements OnInit {
         this.presenter = new FormControl('', Validators.required)
         this.duration = new FormControl('', Validators.required)
         this.level = new FormControl('', Validators.required)
-        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400)])
+        //note: the validators array is just a list of functions that should be run to validate the control
+        this.abstract = new FormControl('', [Validators.required, Validators.maxLength(400), this.restrictedWords(['foo', 'bar'])])
 
         this.newSessionForm = new FormGroup({
             name: this.name,
@@ -51,6 +52,27 @@ export class CreateSessionComponent implements OnInit {
             presenter: formValues.presenter,
             abstract: formValues.abstract,
             voters: []
+        }
+    }
+
+    //custom validator, returns null if valid or error object if not valid
+    //restricted words is a function that returns a function, and that returned function is our validator
+    private restrictedWords(words) {
+        return (control: FormControl): { [key: string]: any } =>
+        {
+            //no validation if words is empty
+            if (!words) return null;
+
+            //loop over all keywords and check if control contains that word, return word, otherwise return null
+            //then filter out the nulls
+            var invalidWords = words.map(w => control.value.includes(w) ? w : null)
+                .filter(w => w != null);
+
+            //if the control's value contains the word 'foo', then it is invalid and we'll return an object with a restrictedWords property
+            // the returned error object typically has a key that matches the validator name (i.e. the name of the function)
+            return invalidWords && invalidWords.length > 0
+                ? {'restrictedWords': invalidWords.join(', ')}
+                : null
         }
     }
 
